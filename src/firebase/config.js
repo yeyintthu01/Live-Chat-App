1f1525996af4c60d3b1724287dff4294d8607121
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-15 16:41:33 +0630

    add firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyDi0hKXXVEpb4Q8Jdb9pS6iZ1R3xcFeiNc",
+    authDomain: "live-chat-app-e8783.firebaseapp.com",
+    projectId: "live-chat-app-e8783",
+    storageBucket: "live-chat-app-e8783.appspot.com",
+    messagingSenderId: "857480657378",
+    appId: "1:857480657378:web:141765ed5f01ed9346a1bb"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let db=firebase.firestore();
+  let timestamp=firebase.firestore.FieldValue.serverTimestamp;
+
+  export {db,timestamp};
